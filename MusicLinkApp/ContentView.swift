ec49a692da93fcf8361338017ad00e48f7fcbfbe
Dhruv Weaver <dhruv.weaver@gmail.com>
2022-06-11 11:23:59 -0400

    initial commit

---
+++ MusicLinkApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicLinkApp
+//
+//  Created by Dhruv Weaver on 6/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicLinkApp/MusicLinkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicLinkAppApp.swift
+//  MusicLinkApp
+//
+//  Created by Dhruv Weaver on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicLinkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
