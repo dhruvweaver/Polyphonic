298ad2b021a49f1f30840a15909ad1e8ad74fe80
Dhruv Weaver <dhruv.weaver@gmail.com>
2022-06-12 00:46:34 -0400

    SongData: start work on Spotify API calls Also added Keys.swift for holding secrets used for API calls

---
+++ MusicLinkApp/Models/Keys.swift
@@ -0,0 +1,13 @@
+//
+//  Keys.swift
+//  MusicLinkApp
+//
+//  Created by Dhruv Weaver on 6/11/22.
+//
+
+import Foundation
+
+let spotifyClientID = "8ed22ce37b76475d931475997d6b5afc"
+let spotifyClientSecret = "d6a4ec99dc2f4e4cac8863b6475b6aae"
+
+let spotifyAuthKey:String = (spotifyClientID + ":" + spotifyClientSecret).toBase64()
---
+++ MusicLinkApp/Models/SongData.swift
@@ -0,0 +1,67 @@
+//
+//  SongData.swift
+//  MusicLinkApp
+//
+//  Created by Dhruv Weaver on 6/11/22.
+//
+
+import Foundation
+
+class SongData {
+    private let starterLink: URL!
+    
+    init(starterLink: URL) {
+        self.starterLink = starterLink
+    }
+    
+    
+    
+    enum Platform {
+        case unknown, spotify, appleMusic
+    }
+    private var starterSource: Platform = Platform.unknown
+    
+    struct RelatedData {
+        private let title: String!
+        private let ISRC: String!
+        private let artist: String!
+        private let album: String!
+        // could later hold an array of links to different platforms. Or a dictionary to quickly find the desired one
+        private let link: URL!
+    }
+    
+    func findPlatform() {
+        let linkString = starterLink.absoluteString
+        if (linkString.contains("apple")) {
+            starterSource = Platform.appleMusic
+        } else if (linkString.contains("spotify")) {
+            starterSource = Platform.spotify
+        }
+    }
+    
+    func getSpotifyAuthKey() async {
+        let url = URL(string: "https://accounts.spotify.com/api/token")!
+        let urlSession = URLSession.shared
+        
+        var request = URLRequest(url: url)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.setValue("Basic \(spotifyAuthKey)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "POST"
+        let parameters: [String: Any] = [
+            "grant_type": "client_credentials"
+        ]
+        
+        
+    }
+    
+    func findTranslatedLink() {
+        // first identify which platform the link starts with
+        findPlatform()
+        
+        if (starterSource == Platform.spotify) {
+            // Spotify API call can be made with the Spotify ID. This is located at the end of a Spotify link
+            let spotifyID = starterLink.lastPathComponent
+            
+        }
+    }
+}
