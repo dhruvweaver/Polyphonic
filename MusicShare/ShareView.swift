cc604375e49c1726aff1625c17c293ae126b5496
Dhruv Weaver <dhruv.weaver@gmail.com>
2022-06-24 15:45:30 -0400

    ShareView: button gets link from extension for translation Needs to do this automatically

--- MusicShare/ShareView.swift
+++ MusicShare/ShareView.swift
@@ -8,18 +8,22 @@
 import SwiftUI
 
 struct ShareView: View {
-    @State private var linkStr: String = "No link"
+    var item: NSExtensionItem!
+    @State private var linkStr: String = "No link yet"
     @State private var linkOut: String = ""
     @State private var isLoading: Bool = false
     var body: some View {
         let songData = SongData()
         VStack(alignment: .center) {
-            Text("Translate links between Apple Music and Spotify")
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-                .padding(.bottom)
+            Button("Get Input Link") {
+                getURL()
+            }
             HStack(alignment: .center) {
-                Text(linkStr)
+                TextField("", text: $linkStr)
+                    .textFieldStyle(.roundedBorder)
+                    .cornerRadius(10)
+                    .overlay(RoundedRectangle(cornerRadius: 10.0).strokeBorder(Color.gray, style: StrokeStyle(lineWidth: 1.0)))
+                    .padding(.horizontal)
             }
             if (!isLoading) {
                 Button("Translate") {
@@ -45,6 +49,32 @@ struct ShareView: View {
             }
             Button("Copy Translated Link") {
                 UIPasteboard.general.string = linkOut
+            }
+        }
+    }
+    
+    init(item: NSExtensionItem) {
+        self.item = item
+        getURL()
+    }
+    
+    func getURL() {
+        if let itemProvider = item.attachments?.first {
+            if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+                itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (url, error) -> Void in
+                    if let error = error {
+                        print("error :-", error)
+                    }
+                    if (url as? NSURL) != nil {
+                        // send url to server to share the link
+                        do {
+                            if let url = url as? URL{
+                                print(url.absoluteString)
+                                linkStr = url.absoluteString
+                            }
+                        }
+                    }
+                })
             }
         }
     }
--- MusicShare/ShareViewController.swift
+++ MusicShare/ShareViewController.swift
@@ -13,15 +13,16 @@ class ShareViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Add the hosting controller as a child view controller
-        let hostingController = UIHostingController(rootView: ShareView())
-        self.addChild(hostingController)
-        self.view.addSubview(hostingController.view)
-        
-        hostingController.view.translatesAutoresizingMaskIntoConstraints=false
-        hostingController.view.topAnchor.constraint(equalTo:view.topAnchor).isActive=true
-        hostingController.view.bottomAnchor.constraint(equalTo:view.bottomAnchor).isActive=true
-        hostingController.view.leftAnchor.constraint(equalTo:view.leftAnchor).isActive=true
-        hostingController.view.rightAnchor.constraint(equalTo:view.rightAnchor).isActive=true
+        if let item = extensionContext?.inputItems.first as? NSExtensionItem {
+            let hostingController = UIHostingController(rootView: ShareView.init(item: item))
+            self.addChild(hostingController)
+            self.view.addSubview(hostingController.view)
+            
+            hostingController.view.translatesAutoresizingMaskIntoConstraints=false
+            hostingController.view.topAnchor.constraint(equalTo:view.topAnchor).isActive=true
+            hostingController.view.bottomAnchor.constraint(equalTo:view.bottomAnchor).isActive=true
+            hostingController.view.leftAnchor.constraint(equalTo:view.leftAnchor).isActive=true
+            hostingController.view.rightAnchor.constraint(equalTo:view.rightAnchor).isActive=true
+        }
     }
 }
