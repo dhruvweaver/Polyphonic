ca06a2d9ae626617328c6f5300fbb2a9f5f61adc
Dhruv Weaver <dhruv.weaver@gmail.com>
2022-07-11 10:58:42 -0400

    Clean up View structure

--- Polyphonic/Views/PlaylistView.swift
+++ Polyphonic/Views/PlaylistView.swift
@@ -11,7 +11,8 @@ struct PlaylistView: View {
     var body: some View {
         NavigationView {
             VStack (alignment: .center) {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text("Coming soon...")
+                    .fontWeight(.bold)
             }
             .navigationTitle("Polyphonic")
         }
